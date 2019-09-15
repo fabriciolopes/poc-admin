73f1d24d9a0ad99d99baf5bb353263c5b1146909
Fabricio Lopes Vital <fabriciolopes@cvccorp.com.br>
2019-09-14 22:17:01 -0300

    add https

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
