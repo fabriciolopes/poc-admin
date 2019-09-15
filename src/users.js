9f0279213a30b459ecbd543d17024ff0ee21bf01
Fabricio Lopes Vital <fabriciolopes@cvccorp.com.br>
2019-09-14 22:09:40 -0300

    initial commit

---
+++ src/users.js
@@ -0,0 +1,16 @@
+// in src/users.js
+import React from 'react';
+
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+        </Datagrid>
+    </List>
+);
